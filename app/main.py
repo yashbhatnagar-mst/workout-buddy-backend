8d3dce497f6271aaeb7aeafa52794bbfed560f63
aarush-mst <aarush.chauhan@metasquaretech.com>
2025-07-29 18:12:49 +0530

    Created Workout Chart Route

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.db.mongodb import db
 from app.utils.gemini import configure_gemini_model
 from app.api.api_v1 import api_router
 from app.api.routes.progress_chart import router as progress_chart_router
+from app.api.routes.workout_charts import router as workout_charts_router
 
 
 app = FastAPI(title=settings.APP_NAME)  
@@ -25,6 +26,7 @@ app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET_KEY)
 # Include all routes from api_v1
 app.include_router(api_router, prefix="/api")
 app.include_router(progress_chart_router, prefix="/api")
+app.include_router(workout_charts_router, prefix="/api")
         
 # Startup
 @app.on_event("startup")
