05457c3e8801d161b80d6ae5ceb62e2976cd5834
aarush-mst <aarush.chauhan@metasquaretech.com>
2025-07-31 21:17:15 +0530

    Removed adherence percent and consistency percent from workout report

--- app/schemas/workout_progress.py
+++ app/schemas/workout_progress.py
@@ -27,7 +27,6 @@ class WorkoutProgressSummary(BaseModel):
     average_rpe: float
     total_sets: int
     total_reps: int
-    calories_burned: int
     dailyLog: List[DailyBurnout]
     sum_of_all_calorie_burnout: int
     muscle_distribution: MuscleDistribution
